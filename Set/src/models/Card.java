fb123c333975b87551474dc32028a7d9118c4133
Luke Rider <luke.rider19@gmail.com>
2016-10-23 22:53:12 -0400

    Card Model Functionality
    This class is a representation of a single card in the game Set. This
    commit includes a build out of all the attributes that are needed to
    model a card; as such it has four properties (Color, Fill, Shape,
    Number). These properties can only be changed at the instantiation of an
    instance of a class, while there are access methods the properties can
    not be changed once the card is created.

--- Set/src/models/Card.java
+++ Set/src/models/Card.java
@@ -1,5 +1,29 @@
 package models;
 
 public class Card {
-
+private String Color, Fill, Shape;
+private int Number;
+////////////////////////Constructor Methods////////////
+public Card(String tempColor,String tempFill,String tempShape, int tempNumber){
+	Color = tempColor;
+	Fill = tempFill;
+	Shape = tempShape;
+	Number = tempNumber;
+}//Ends Constructor 
+///////////////////////Getter Methods//////////////////
+public String getColor(){
+	return Color;
+}//Ends getColor()
+public String getFill(){
+	return Fill;
+}//Ends getFill()
+public String getShape(){
+	return Shape;
+}//Ends getShape()
+public int getNumber(){
+	return Number;
+}//Ends getNumber()
+/*
+ * Did not create "Setter Methods" because once cards are created its properties should not be changed. 
+ */
 }
