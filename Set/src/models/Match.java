f37b19d766d9289514fd4e6d7cd803244f5930d5
Luke Rider <luke.rider19@gmail.com>
2016-10-25 08:21:35 -0400

    Match Model Funtionality
    This class Is a representation of Match in the game set. This commit
    includes a built out of all the attributes of a match. As such, this
    class takes an array of cards (which should only ever contain three
    cards). You can change the set given at instantiation, and check to see
    if the passed array of cards is a match.

--- Set/src/models/Match.java
+++ Set/src/models/Match.java
@@ -1,5 +1,107 @@
 package models;
-
+/*
+ * Class Precondition, The cardSet array should only ever contain three cards.
+ */
 public class Match {
+private boolean isMatch;
+private Card[] cardSet;
+////////////////////////Constructor Methods////////////
+public Match (Card[] tempCardArray){
+	cardSet = tempCardArray.clone();
 
+}
+////////////////////////Public Methods/////////////////
+/*
+ * precondition: there are only 3 cards in the cardSet array.
+ */
+public boolean isAMatch(){
+	if(compairFill() && compairColor() && compairShape() && compairNumber()){
+		isMatch = true;
+	}
+	else{
+		isMatch = false;
+	}
+	return isMatch;
+}//End isAMatch
+public void newCards(Card[] newCardSet){
+	cardSet = newCardSet.clone();
+}
+////////////////////////Private Methods////////////////
+/*
+ * precondition: there are only 3 cards in the cardSet array.
+ * this method compares the Fill attribute among 3 Cards
+ */
+private boolean compairFill(){
+	boolean result;
+	if(cardSet[0].getFill().equals(cardSet[1].getFill()) && cardSet[1].getFill().equals(cardSet[2].getFill())){
+		result = true;
+	}
+	else if(!cardSet[0].getFill().equals(cardSet[1].getFill()) && !cardSet[1].getFill().equals(cardSet[2].getFill()) &&
+			!cardSet[0].getFill().equals(cardSet[2].getFill())){
+		result = true;
+	}
+	else{
+		result = false;
+	}
+	return result;
+}//end CompairFill()
+
+/*
+ * precondition: there are only 3 cards in the cardSet array.
+ * this method compares the Color attribute among 3 Cards
+ */
+private boolean compairColor(){
+	boolean result;
+	if(cardSet[0].getColor().equals(cardSet[1].getColor()) && cardSet[1].getColor().equals(cardSet[2].getColor())){
+		result = true;
+	}
+	else if(!cardSet[0].getColor().equals(cardSet[1].getColor()) && !cardSet[1].getColor().equals(cardSet[2].getColor()) &&
+			!cardSet[0].getColor().equals(cardSet[2].getColor())){
+		result = true;
+	}
+	else{
+		result = false;
+	}
+	return result;
+	
+}//end compairColor()
+
+/*
+ * precondition: there are only 3 cards in the cardSet array.
+ * this method compares the Shape attribute among 3 Cards
+ */
+private boolean compairShape(){
+	boolean result;
+	if(cardSet[0].getShape().equals(cardSet[1].getColor()) && cardSet[1].getShape().equals(cardSet[2].getShape())){
+		result = true;
+	}
+	else if(!cardSet[0].getShape().equals(cardSet[1].getShape()) && !cardSet[1].getShape().equals(cardSet[2].getShape()) &&
+			!cardSet[0].getShape().equals(cardSet[2].getShape())){
+		result = true;
+	}
+	else{
+		result = false;
+	}
+	return result;
+	
+}//end compairShape()
+
+/*
+ * precondition: there are only 3 cards in the cardSet array.
+ * this method compares the Number attribute among 3 Cards
+ */
+private boolean compairNumber(){
+	boolean result;
+	if(cardSet[1].getNumber() == cardSet[0].getNumber() && cardSet[0].getNumber() ==cardSet[2].getNumber()){
+		result = true;
+	}
+	else if(cardSet[0].getNumber() != cardSet[1].getNumber() && cardSet[1].getNumber() != cardSet[2].getNumber() &&
+			cardSet[0].getNumber() != cardSet[2].getNumber()){
+		result = true;
+	}
+	else {
+		result = false;
+	}
+	return result;
+}//end compare Number
 }
